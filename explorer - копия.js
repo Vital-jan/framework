735cde5254d837626aaa6aea57287c988b47ad2e
Explorer <vitaljan@gmail.com>
2018-09-01 14:30:27 +0300

    version_02

--- explorer - копия.js
+++ explorer - копия.js
@@ -1,6 +1,6 @@
 function writeRound(s, x, y, size, radius, start, end, userStyle) {
     // возвращаеи html-код для отрисовки текста по радиусу.
-    // writeRound (<текст>,<коорд x>,<коорд y>,<разм. шрифта>,<радиус>,<нач. угол>,<кон. угол>, <направление> [,<польз. css>])
+    // writeRound (<текст>,<коорд x>,<коорд y>,<разм. шрифта>,<радиус>,<нач. угол>,<кон. угол>[,<польз. css>])
     // s - строка текста;
     // x, y - координаты центра окружности базовой линии текста в px;
     // size - размер шрифта в px;
@@ -8,24 +8,19 @@ function writeRound(s, x, y, size, radius, start, end, userStyle) {
     // start, end - начальный и конечный угол сегмента окружности базовой линии текста в deg;
     // userStyle - необязательный параметр - пользовательский css стиль;
     var t = '';
-    var direction = start < end ? 1 : -1;
     var charStyle = `
         position: absolute;
         text-align:center;
         width: ${size}px;
         height: ${size}px;
         font-size:${size}px;
-        top: ${y + radius * -direction - size / 2}px;
-        left: ${x - size / 2}px;
-        transform-origin: ${size / 2}px ${radius * direction + size / 2}px;
-        border: 1px solid black;
-        margin: 0;
-        padding: 0;
-        `;
-    var deg = start - 180 * (direction - 1) / -2;
+        top: ${y}px;
+        left: ${x}px;
+        transform-origin: ${size/2}px ${radius}px;`;
+    var deg = start;
     var degPlus = (end-start)/(s.length-1);
     for (var n = 0; n < s.length; n++) {
-        var charStyle2 =` transform: rotate(${deg}deg);`;
+        var charStyle2 = ` transform: rotate(${deg}deg);`;
         deg += degPlus;
         t += `<div style='${charStyle} ${charStyle2} ${userStyle}'>${s[n]}</div>`;
     }
